3d159a8103c17a71abf76058ffb82c9a1fa8afb4
Alex Modroño <ialex11@engineer.com>
2019-05-19 12:24:18 +0200

    message server updated

--- backends/messageServer.py
+++ backends/messageServer.py
@@ -2,17 +2,38 @@
 # Message Receiver
 import os
 from socket import *
-host = ""
-port = 13000
-buf = 1024
-addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
-UDPSock.bind(addr)
-print("Waiting to receive messages...")
-while True:
-    (data, addr) = UDPSock.recvfrom(buf)
-    print("Received message: " + data)
-    if data == "exit":
-        break
-UDPSock.close()
-os._exit(0)
+import halo as h
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+def main(host):
+    host = ""
+    port = 13000
+    buf = 1024
+    addr = (host, port)
+    UDPSock = socket(AF_INET, SOCK_DGRAM)
+    UDPSock.bind(addr)
+    print("Waiting to receive messages...")
+    while True:
+        (data, addr) = UDPSock.recvfrom(buf)
+        print("Received message: " + data)
+        if data == "exit":
+            break
+    UDPSock.close()
+    os._exit(0)
+
+try:
+    if len(sys.argv[1]) >= 1:
+        main(sys.argv[1])
+except:
+    print(color.RED + "Enrer")
